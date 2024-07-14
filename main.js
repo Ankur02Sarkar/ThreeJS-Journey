cbaac7154035630f9d1234ed453f5e4f50e84834
Ankur-Raphacure <ankur@raphacure.com>
2024-07-14 11:39:10 +0530

    full screen

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
-// renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
